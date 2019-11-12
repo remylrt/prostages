9658801455b4cb7339c2ab582f5a346618836a74
Remy Lartiguelongue <rlartiguelon@iutbayonne.univ-pau.fr>
2019-11-12 16:32:47 +0100

    Création des routes avec affichage brut des réponses

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-class ProstagesController extends AbstractController
-{
-    /**
-     * @Route("/prostages", name="prostages")
-     */
-    public function index()
-    {
-        return $this->render('prostages/index.html.twig', [
-            'controller_name' => 'ProstagesController',
-        ]);
+class ProstagesController extends AbstractController{
+
+    public function index(){
+        return new Response("<html><body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");
+    }
+
+    public function entreprises(){
+        return new Response("<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
+    }
+
+    public function formations(){
+        return new Response("<html><body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
+    }
+
+    public function stages($id){
+        return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant " . $id . "</h1></body></html>");
     }
 }
