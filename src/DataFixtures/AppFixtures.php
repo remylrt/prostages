c989301e7165073143b07fc75aa7f404efc89021
Remy Lartiguelongue <rlartiguelon@iutbayonne.univ-pau.fr>
2019-12-20 12:28:26 +0100

    Création des données de test pour toutes les tables

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Entreprise;
+use App\Entity\Formation;
+use App\Entity\Stage;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,13 +12,43 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $entreprise = new Entreprise();
-        $entreprise->setNom("Capgemini");
-        $entreprise->setAdresse("Rue Joseph Szydlowski, 64100 Bayonne");
-        $entreprise->setSite("www.capgemini.com");
-        $entreprise->setActivite("ESN");
-        $entreprise->setTel("0533783300");
-        $manager->persist($entreprise);
+        $faker = \Faker\Factory::create('fr_FR');
+        $dutinfo = new Formation;
+        $duttic = new Formation;
+        $licenceMulti = new Formation;
+
+        //Génération des données pour la table Entreprise
+        for ($i=0; $i < 10; $i++) { 
+            $entreprise = new Entreprise();
+            $entreprise->setNom($faker->company);
+            $entreprise->setAdresse($faker->address);
+            $entreprise->setSite($faker->domainName);
+            $entreprise->setActivite($faker->jobTitle);
+            $entreprise->setTel($faker->regexify('0[1-5][0-9]{8}'));
+            $manager->persist($entreprise);
+        }
+        
+        //Génération des données pour la table Stage
+        for ($i=0; $i < 30; $i++) { 
+            $stage = new Stage;
+            $stage->setTitre($faker->catchPhrase);
+            $stage->setMail($faker->companyEmail);
+            $stage->setDescription($faker->realText($maxNbChars = 255, $indexSize = 2));
+            $manager->persist($stage);
+        }
+
+        //Création des données pour la table Formation
+        $dutinfo->setNom("Diplome Universitaire de Technologie Informatique");
+        $dutinfo->setSigle("DUT Informatique");
+        $manager->persist($dutinfo);
+
+        $duttic->setNom("Diplome Universitaire de Technologie Technologie de l'Information et de la Communication");
+        $duttic->setSigle("DUT TIC");
+        $manager->persist($duttic);
+
+        $licenceMulti->setNom("Licence professionnelle Multimedia");
+        $licenceMulti->setSigle("LP Multimedia");
+        $manager->persist($licenceMulti);
 
         $manager->flush();
     }
