177986d566830993a4c6f4ab01b54d0c4328760e
Remy Lartiguelongue <rlartiguelon@iutbayonne.univ-pau.fr>
2019-12-20 11:39:10 +0100

    Création d'une donnée de test et installation de faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Entreprise;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $entreprise = new Entreprise();
+        $entreprise->setNom("Capgemini");
+        $entreprise->setAdresse("Rue Joseph Szydlowski, 64100 Bayonne");
+        $entreprise->setSite("www.capgemini.com");
+        $entreprise->setActivite("ESN");
+        $entreprise->setTel("0533783300");
+        $manager->persist($entreprise);
 
         $manager->flush();
     }
