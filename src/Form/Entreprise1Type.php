b6f7a9748039e3b0f478ddf9ea3982720e121dd3
Remy Lartiguelongue <rlartiguelon@iutbayonne.univ-pau.fr>
2020-03-24 12:00:41 +0100

    Mise en place du CRUD

---
+++ src/Form/Entreprise1Type.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Entreprise;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Entreprise1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('adresse')
+            ->add('site')
+            ->add('activite')
+            ->add('tel')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Entreprise::class,
+        ]);
+    }
+}
